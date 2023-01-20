f010a503e4f8cca6ed1b7b734cbe4d65c8f853cb
AlyaAlnuaimi <aliaalnuaimi111@gmail.com>
2023-01-20 11:45:18 +0400

    php

--- online-shop/admin/index.php
+++ online-shop/admin/index.php
@@ -1,34 +1,42 @@
-<?php 
+<?php
+define('BASE_ADMIN_URL', '/online-shop/admin/');
+define('BASE_URL', '/online-shop/');
+define('BASE_PATH', '../');
+require_once('../logic/authentication.php');
+protectAdmin();
 require_once('./layouts/header.php');
 ?>
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1 class="m-0">Home</h1>
-              </div>
-            </div>
-          </div>
-          <!-- /.container-fluid -->
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Home</h1>
         </div>
-        <!-- /.content-header -->
 
-        <!-- Main content -->
-        <section class="content">
-          <div class="container-fluid">
-            <div class="row">
-              <div class="col-12">
-
-              </div>
-            </div>
-          </div>
-          <!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
       </div>
- <?php
- require_once('./layouts/footer.php')
- ?>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+
+        </div>
+      </div>
+
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<?php
+require_once('./layouts/footer.php');
+?>
